c9751d1c8e22e89fd7708b05af90485717653249
Francisco Duran <francisco.duran@koombea.com>
2018-11-26 10:54:04 -0500

    Add changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import CommentDetail from './Components/commentDetail';
 import './App.css';
 
 class App extends Component {
@@ -7,19 +7,11 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <div className="ui container commnets">
+          <CommentDetail />
+          <h2>Hello!</h2>
+        </div>  
       </div>
     );
   }
