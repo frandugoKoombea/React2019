d382017da5f3d6095b1d37c33db0a55379c5914d
Francisco Duran <francisco.duran@koombea.com>
2018-12-01 11:41:48 -0500

    Add changes i diont know

--- src/App.js
+++ src/App.js
@@ -4,12 +4,26 @@ import ApprovalCard from './Components/ApprovalCard';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super();
+    this.state = { lat: null , errorMessage: '' };
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message })
+      }
+    );
+  }
+
   render() {
     return (
       <div className="App">
         <div className="ui container commnets">
           <ApprovalCard children="sias" >
-            <h2>Warning!</h2>
+            <h2>Location is: { this.state.lat }</h2>
+            <span>Error: { this.state.errorMessage }</span>
             <p>Lorem ipsum dolor yeah</p>
             <CommentDetail date="Today at 3:00PM" description="lorem ipsum 1" />
           </ApprovalCard>
